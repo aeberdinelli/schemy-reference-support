c3e86f2d346cf8a9875e0f5d03bc003e5d8f2469
Alan Berdinelli <alan.berdinelli@clinifyhealth.com>
2021-08-28 16:18:00 -0300

    Init repo

---
+++ README.md
@@ -0,0 +1,36 @@
+# Reference support
+## Introduction 
+This plugin adds the functionality to reference values from within a schema using a new method `Schemy.$ref()`.
+
+## Example
+```javascript
+const Schemy = require('schemy');
+const SchemyReferenceSupport = require('schemy-reference-support');
+
+// Load the plugin into Schemy
+Schemy.extend(SchemyReferenceSupport);
+
+// Now you can do something like this
+const schema = new Schemy({
+    maxItems: {
+        type: Number,
+        required: true
+    },
+    items: {
+        type: [String],
+        max: Schemy.$ref('maxItems')
+    }
+});
+
+schema.validate({
+	maxItems: 1,
+    items: ['one','two']
+}); // => false
+
+schema.getValidationErrors(); // => [ 'Property items must contain no more than 1 elements' ]
+```
+
+In the example above, we define a property `maxItem` as a required number. Then, we set the max items for `items` to be that property. This way, since we pass `1` to `maxItems`, the schema will fail when passing more than 1 item to the array.
+
+## Notes
+- Requires Schemy version **>= 3.2.0**
---
+++ index.js
@@ -0,0 +1,180 @@
+const Plugin = {
+	pluginName: 'schemy-reference-support',
+
+	// Required Schemy version
+	requiredVersion: '3.2.0',
+
+	// Things where you can reference other schema properties
+	availableRefs: ['min', 'max', 'regex'],
+
+	versionCheck() {
+        if (!Plugin.Schemy || !Plugin.Schemy.getVersion) {
+            throw new Error(
+                `Schemy object is not currently available within the plugin. Check you are using schemy version ${Plugin.requiredVersion} or above`
+            );
+        }
+
+        const [ major, minor ] = Plugin.Schemy.getVersion().split(/\./g).map(value => parseInt(value));
+        const [ reqMajor, reqMinor ] = Plugin.requiredVersion.split(/\./g).map(value => parseInt(value));
+
+        if (major < reqMajor || (major === reqMajor && minor < reqMinor)) {
+            throw new Error(`Child Settings plugin requires schemy version ${Plugin.requiredVersion} or above`);
+        }
+    },
+
+	parseReferenceName(str) {
+		return str.substr(5);
+	},
+
+	isReference(str) {
+		return str.startsWith('$ref.');
+	},
+
+	pluginsInitialized(plugins) {
+		// Initialize plugin only once
+		if (!plugins.some(plugin => plugin.pluginName === Plugin.pluginName)) {
+			return;
+		}
+
+		// Check compatibility
+		Plugin.versionCheck();
+
+		Plugin.Schemy.$ref = (property, data = null) => {
+			if (data) {
+				let value = null;
+
+				property = Plugin.parseReferenceName(property);
+
+				try {
+					value = eval(`data.${property}`);
+				} catch (err) {
+					throw `Could not get referenced value ${property}`;
+				}
+
+				return value;
+			}
+
+			return `$ref.${property}`;
+		};
+	},
+
+	/**
+	 * This is the same logic as the parsing method from Schemy
+	 * The only difference is that we allow Schemy.$ref inside the schema now
+	 */
+	beforeParse(schema) {
+		// Always check compatibility
+		Plugin.versionCheck();
+
+		// Prevent schema from being parsed twice
+		this.schemaParsed = true;
+
+		for (var [key, properties] of Object.entries(schema)) {
+			if (key !== 'required' && !properties.type) {
+				if (typeof properties === 'function' || properties === 'uuid/v1' || properties === 'uuid/v4') {
+					schema[key] = { type: properties, required: true };
+				}
+				
+				else if (typeof properties === 'object') {
+					try {
+						const parsed = {};
+
+						if (schema[key].custom) {
+							const { custom } = schema[key];
+							parsed.custom = custom;
+						}
+
+						parsed.type = new Plugin.Schemy(properties);
+						parsed.required = !!properties.required;
+
+						schema[key] = parsed;
+					} catch (err) {
+						throw `Could not parse property ${key} as schema`;
+					}
+				}
+			}
+
+			else if (typeof properties.type === 'function') {
+				if (['boolean','string','number','object','function'].indexOf(typeof properties.type()) === -1) {
+					throw `Unsupported type on ${key}: ${typeof properties.type()}`;
+				}
+
+				if (typeof properties.type() !== 'string' && (properties.enum || properties.regex)) {
+					throw `Invalid schema for ${key}: regex and enum can be set only for strings`;
+				}
+
+				if (properties.regex && !(properties.regex instanceof RegExp) && !Plugin.isReference(properties.regex)) {
+					throw `Invalid schema for ${key}: regex must be an instance of RegExp`;
+				}
+
+				if (properties.min && typeof properties.min !== 'number' && !Plugin.isReference(properties.min)) {
+					throw `Invalid schema for ${key}: min property must be a number`;
+				}
+
+				if (properties.max && typeof properties.max !== 'number' && !Plugin.isReference(properties.max)) {
+					throw `Invalid schema for ${key}: max property must be a number`;
+				}
+			}
+
+			else if (typeof properties.type === 'string' && ['uuid/v1','uuid/v4'].indexOf(properties.type) === -1) {
+				throw `Unsupported type on ${key}: ${properties.type}`;
+			}
+
+			else if (typeof properties.type === 'object' && Array.isArray(properties.type)) {
+				if (properties.type.length > 1) {
+					throw `Invalid schema for ${key}. Array items must be declared of any type, or just one type: [String], [Number]`;
+				}
+
+				// Auto parse array item as schemy
+				if (typeof properties.type[0] === 'object') {
+					if (typeof properties.type[0].validate === 'undefined') {
+						properties.type[0] = new Plugin.Schemy(properties.type[0]);
+					}
+				}
+			}
+
+			// Parse child schema and keep custom validator if it exists
+			else if (typeof properties.type === 'object' && (typeof properties.type.validate === 'undefined')) {
+				try {
+					const parsed = {};
+
+					if (schema[key].custom) {
+						const { custom } = schema[key];
+						parsed.custom = custom;
+					}
+
+					parsed.type = new Plugin.Schemy(properties.type);
+					parsed.required = !!properties.required;
+
+					schema[key] = parsed;
+				} catch (err) {}
+			}
+
+			if (properties.custom && typeof properties.custom !== 'function') {
+				throw `Custom validator for ${key} must be a function, was ${typeof properties.custom}`;
+			}
+		}
+	},
+
+	/**
+	 * Fill the referenced values before validating
+	 */
+	beforeValidate(data) {
+		Plugin.versionCheck();
+
+		if (!data || typeof data !== 'object') {
+			this.validationErrors.push('Data passed to validate is incorrect. It must be an object.');
+			return false;
+		}
+
+		for (var [key, properties] of Object.entries(this.schema)) {
+			Plugin.availableRefs.forEach(ref => {
+				if (properties[ref]) {
+					properties[ref] = Plugin.Schemy.$ref(properties[ref], data);
+				}
+			});
+		}
+	}
+}
+
+module.exports = Plugin;
